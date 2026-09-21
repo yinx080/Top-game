5af93b3d7481c5233921e3682a9b8c8b99efce7a
ignacio_almonte_santos <lilsaint080@gmail.com>
2026-09-21 16:44:11 +0200

    volviendo al commit anterior

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
-import { dismissBoot } from './lib/boot' // NEW
 import { sfx } from './lib/sfx'
 import './styles/global.css'
 
@@ -17,5 +16,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-
-void dismissBoot() // NEW
